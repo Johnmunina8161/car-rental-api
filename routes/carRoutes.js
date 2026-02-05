929c676733690c48db49dc659e83e47cc55d6b58
Your Name <you@example.com>
2026-02-05 20:48:40 +0200

    commit

--- routes/carRoutes.js
+++ routes/carRoutes.js
@@ -1,15 +1,1 @@
-const router = require('express').Router();
-const auth = require('../middleware/auth');
-const role = require('../middleware/role');
-const controller = require('../controllers/carController');
 
-// Public
-router.get('/', controller.getAllCars);
-router.get('/:id', controller.getCarById);
-
-// Admin only
-router.post('/', auth, role('admin'), controller.createCar);
-router.put('/:id', auth, role('admin'), controller.updateCar);
-router.delete('/:id', auth, role('admin'), controller.deleteCar);
-
-module.exports = router;
